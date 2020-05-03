84e47b176f92b086582502315ff7a5ea8859e49d
Espen Zachrisen <espen.zachrisen@alticeusa.com>
2020-05-03 13:15:10 -0500

    Struggling with actions

--- cel/proto_test.go
+++ cel/proto_test.go
@@ -11,6 +11,55 @@ import (
 	"github.com/golang/protobuf/ptypes/timestamp"
 )
 
+// func TestConstruction(t *testing.T) {
+
+// 	schema := rules.Schema{
+// 		Elements: []rules.DataElement{
+// 			{Name: "student", Type: rules.Proto{Protoname: "school.Student", Message: &school.Student{}}},
+// 			{Name: "now", Type: rules.Timestamp{}},
+// 		},
+// 	}
+
+// 	engine := cel.NewEngine()
+
+// 	ruleSet := rules.RuleSet{
+// 		ID:     "student_actions",
+// 		Schema: schema,
+// 		Rules: map[string]rules.Rule{
+// 			"construct": rules.SimpleRule{Expr: `school.Student{ GPA: 1.0}`},
+// 		},
+// 	}
+
+// 	err := engine.AddRuleSet(ruleSet)
+// 	if err != nil {
+// 		t.Errorf("Error adding ruleset: %v", err)
+// 	}
+
+// 	s := school.Student{
+// 		Age:            16,
+// 		GPA:            3.76,
+// 		Status:         school.Student_ENROLLED,
+// 		Grades:         []float64{4.0, 4.0, 3.7},
+// 		Attrs:          map[string]string{"Nickname": "Joey"},
+// 		EnrollmentDate: &timestamp.Timestamp{Seconds: time.Date(2010, 5, 1, 12, 12, 59, 0, time.FixedZone("UTC-8", -8*60*60)).Unix()},
+// 	}
+
+// 	data := map[string]interface{}{
+// 		"student": &s,
+// 		"now":     &timestamp.Timestamp{Seconds: time.Now().Unix()},
+// 	}
+
+// 	results, err := engine.EvaluateAll(data, "student_actions")
+// 	if err != nil {
+// 		t.Errorf("Evaluation error: %v", err)
+// 	}
+
+// 	for _, v := range results {
+// 		fmt.Println(v.RawValue)
+// 	}
+
+// }
+
 func TestProto(t *testing.T) {
 
 	//	pb.DefaultDb.RegisterMessage(&school.Student{})
