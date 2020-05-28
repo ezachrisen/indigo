4b08af0e1bf6f2eb11a7f04234cb667120e0df2e
Espen Zachrisen <ezachrisen@gmail.com>
2020-05-27 22:56:43 -0500

    Rule options, inheritance

--- cel/cel_test.go
+++ cel/cel_test.go
@@ -1,9 +1,7 @@
 package cel_test
 
 import (
-	"fmt"
 	"log"
-	"strings"
 	"testing"
 	"time"
 
@@ -12,55 +10,12 @@ import (
 	"github.com/ezachrisen/rules/testdata/school"
 	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/google/cel-go/common/types/pb"
+	"github.com/matryer/is"
+	// to enable easy checks for map membership
 )
 
-func TestBasicRules(t *testing.T) {
-
-	engine := cel.NewEngine()
-
-	education := rules.Schema{
-		Elements: []rules.DataElement{
-			{Name: "student.ID", Type: rules.String{}},
-			{Name: "student.Age", Type: rules.Int{}},
-			{Name: "student.GPA", Type: rules.Float{}},
-			{Name: "student.Adjustment", Type: rules.Float{}},
-			{Name: "student.Status", Type: rules.String{}},
-			{Name: "student.Grades", Type: rules.List{ValueType: rules.String{}}},
-			{Name: "student.EnrollmentDate", Type: rules.String{}},
-			{Name: "now", Type: rules.String{}},
-			{Name: "alsoNow", Type: rules.Timestamp{}},
-		},
-	}
-
-	myref := "d04ab6d9-f59d-9474-5c38-34d65380c612"
-	rule := rules.Rule{
-		ID:     "student_actions",
-		Meta:   myref,
-		Schema: education,
-		Rules: map[string]rules.Rule{
-			"honors_student": {
-				ID:   "honors_student",
-				Expr: `student.GPA >= 3.6 && student.Status!="Probation" && !("C" in student.Grades)`,
-			},
-			"at_risk": {
-				ID:   "at_risk",
-				Expr: `student.GPA < 2.5 || student.Status == "Probation"`,
-				Rules: map[string]rules.Rule{
-					"risk_factor": {
-						ID:   "risk_factor",
-						Expr: `2.0+6.0`,
-					},
-				},
-			},
-		},
-	}
-
-	err := engine.AddRule(rule)
-	if err != nil {
-		t.Error(err)
-	}
-
-	data := map[string]interface{}{
+func makeStudentData() map[string]interface{} {
+	return map[string]interface{}{
 		"student.ID":             "12312",
 		"student.Age":            16,
 		"student.GPA":            2.2,
@@ -72,54 +27,10 @@ func TestBasicRules(t *testing.T) {
 		"specificTime":           &timestamp.Timestamp{Seconds: time.Now().Unix()},
 	}
 
-	results, err := engine.EvaluateN(data, "student_actions", 2)
-	// PrintResults(results, 0)
-	// if results.XRef == nil {
-	// 	t.Errorf("No xref. Expected %s", myref)
-	// }
-	if results.Meta != myref {
-		t.Errorf("Expected Xref %v, got %v (type %T)", myref, results.Meta, results.Meta)
-	}
-
-	if results.Pass != true {
-		t.Errorf("Expected true, got false: %v", results.RuleID)
-	}
-
-	if results.Results["honors_student"].Pass != false {
-		t.Errorf("Expected false, got true: %v", "honors_student")
-	}
-
-	if results.Results["at_risk"].Pass != true {
-		t.Errorf("Expected true, got false: %v", "at_risk")
-	}
-
-	if results.Results["at_risk"].Results["risk_factor"].Value.(float64) != 8.0 {
-		t.Errorf("Expected %f, got false: %v", 8.0, results.Results["at_risk"].Results["risk_factor"].Value)
-
-	}
 }
 
-func PrintResults(res *rules.Result, tabs int) {
-
-	fmt.Printf("%-30s %v ", fmt.Sprintf("%s%s", strings.Repeat(" ", tabs), res.RuleID), res.Pass)
-	// r := *res.Rule
-	// test_rule := r.(Rule)
-	// if res.Value != test_rule.expected.Value {
-	// 	fmt.Printf(" --- expected %v (%T), got %v (%T)\n", test_rule.expected.Value, test_rule.expected.Value, res.Value, res.Value)
-	// } else {
-	fmt.Printf("\n")
-	//	}
-
-	for _, cres := range res.Results {
-		PrintResults(&cres, tabs+1)
-	}
-}
-
-func TestCalculation(t *testing.T) {
-
-	engine := cel.NewEngine()
-
-	education := rules.Schema{
+func makeEducationSchema() rules.Schema {
+	return rules.Schema{
 		Elements: []rules.DataElement{
 			{Name: "student.ID", Type: rules.String{}},
 			{Name: "student.Age", Type: rules.Int{}},
@@ -133,92 +44,169 @@ func TestCalculation(t *testing.T) {
 		},
 	}
 
-	data := map[string]interface{}{
-		"student.ID":             "12312",
-		"student.Age":            16,
-		"student.GPA":            2.2,
-		"student.Adjustment":     1.6,
-		"student.Status":         "Enrolled",
-		"student.Grades":         []interface{}{"A", "B", "A"},
-		"student.EnrollmentDate": "2018-08-03T16:00:00-07:00",
-		"now":                    "2019-08-03T16:00:00-07:00",
-	}
-
-	v, err := engine.Calculate(data, `2.0+student.GPA + (1.344 * student.Adjustment)/3.3`, education)
-	if err != nil {
-		t.Error(err)
-	}
-	if v != 4.851636363636364 {
-		t.Errorf("Expected %f, got %f", 4.851636363636364, v)
-	}
 }
 
-func BenchmarkCalculation(b *testing.B) {
-
-	engine := cel.NewEngine()
-
-	education := rules.Schema{
-		Elements: []rules.DataElement{
-			{Name: "student.ID", Type: rules.String{}},
-			{Name: "student.Age", Type: rules.Int{}},
-			{Name: "student.GPA", Type: rules.Float{}},
-			{Name: "student.Adjustment", Type: rules.Float{}},
-			{Name: "student.Status", Type: rules.String{}},
-			{Name: "student.Grades", Type: rules.List{ValueType: rules.String{}}},
-			{Name: "student.EnrollmentDate", Type: rules.String{}},
-			{Name: "now", Type: rules.String{}},
+func makeEducationRules() []rules.Rule {
+	rule1 := rules.Rule{
+		ID:     "student_actions",
+		Meta:   "d04ab6d9-f59d-9474-5c38-34d65380c612",
+		Schema: makeEducationSchema(),
+		Rules: map[string]rules.Rule{
+			"a": {
+				ID:   "honors_student",
+				Expr: `student.GPA >= 3.6 && student.Status!="Probation" && !("C" in student.Grades)`,
+			},
+			"b": {
+				ID:   "at_risk",
+				Expr: `student.GPA < 2.5 || student.Status == "Probation"`,
+				Rules: map[string]rules.Rule{
+					"c": {
+						ID:   "risk_factor",
+						Expr: `2.0+6.0`,
+					},
+				},
+			},
 		},
 	}
 
-	data := map[string]interface{}{
-		"student.ID":             "12312",
-		"student.Age":            16,
-		"student.GPA":            2.2,
-		"student.Adjustment":     1.6,
-		"student.Status":         "Enrolled",
-		"student.Grades":         []interface{}{"A", "B", "A"},
-		"student.EnrollmentDate": "2018-08-03T16:00:00-07:00",
-		"now":                    "2019-08-03T16:00:00-07:00",
+	rule2 := rules.Rule{
+		ID:     "depthRules",
+		Schema: makeEducationSchema(),
+		Expr:   `student.GPA > 3.5`, // false
+		Rules: map[string]rules.Rule{
+			"a": {
+				ID:   "c1",
+				Expr: `student.Adjustment > 0.0`, // true
+			},
+			"b": {
+				ID:   "c2",
+				Expr: `student.Adjustment > 3.0`, // false
+			},
+			"c": {
+				ID:   "c3",
+				Expr: `student.Adjustment < 2.6`, // true
+			},
+			"d": {
+				ID:   "c4",
+				Expr: `student.Adjustment > 3.0`, // false
+			},
+		},
 	}
 
-	for i := 0; i < b.N; i++ {
-		_, err := engine.Calculate(data, `2.0+student.GPA + (1.344 * student.Adjustment)/3.3`, education)
-		if err != nil {
-			b.Fatalf("Could not calculate risk factor: %v", err)
-		}
+	rule3 := rules.Rule{
+		ID:     "ruleOptions",
+		Schema: makeEducationSchema(),
+		Expr:   `student.GPA > 3.5`, // false
+		Rules: map[string]rules.Rule{
+			"A": {
+				ID:   "D",
+				Expr: `student.Adjustment > 0.0`,                         // true
+				Opts: []rules.Option{rules.StopFirstPositiveChild(true)}, // RULE OPTION
+				Rules: map[string]rules.Rule{
+					"d1": {
+						ID:   "d1",
+						Expr: `student.Adjustment < 2.6`, // true
+					},
+					"d2": {
+						ID:   "d2",
+						Expr: `student.Adjustment > 3.0`, // false
+					},
+					"d3": {
+						ID:   "d3",
+						Expr: `student.Adjustment < 2.6`, // true
+					},
+				},
+			},
+			"B": {
+				ID:   "b1",
+				Expr: `student.Adjustment > 3.0`, // false
+			},
+			"E": {
+				ID:   "E",
+				Expr: `student.Adjustment > 0.0`, // true
+				Opts: []rules.Option{},           // NO RULE OPTION
+				Rules: map[string]rules.Rule{
+					"e1": {
+						ID:   "e1",
+						Expr: `student.Adjustment < 2.6`, // true
+					},
+					"e2": {
+						ID:   "e2",
+						Expr: `student.Adjustment > 3.0`, // false
+					},
+					"e3": {
+						ID:   "e3",
+						Expr: `student.Adjustment < 2.6`, // true
+					},
+				},
+			},
+		},
 	}
+
+	return []rules.Rule{rule1, rule2, rule3}
+
 }
 
-func TestProtoMessage(t *testing.T) {
+func TestBasicRules(t *testing.T) {
 
-	pb.DefaultDb.RegisterMessage(&school.Student{})
+	is := is.New(t)
 
-	schema := rules.Schema{
+	engine := cel.NewEngine()
+	rule := makeEducationRules()
+
+	err := engine.AddRule(rule...)
+	is.NoErr(err)
+
+	results, err := engine.Evaluate(makeStudentData(), "student_actions")
+
+	//	rules.PrintResults(results)
+
+	is.NoErr(err)
+	is.Equal(results.Meta, rule[0].Meta)
+	is.True(results.Pass)
+	is.True(!results.Results["honors_student"].Pass)
+	is.True(results.Results["at_risk"].Pass)
+	is.Equal(results.Results["at_risk"].Results["risk_factor"].Value.(float64), 8.0)
+
+}
+
+func TestCalculation(t *testing.T) {
+
+	is := is.New(t)
+	engine := cel.NewEngine()
+	v, err := engine.Calculate(makeStudentData(), `2.0+student.GPA + (1.344 * student.Adjustment)/3.3`, makeEducationSchema())
+
+	is.NoErr(err)
+	is.Equal(v, 5.055272727272728)
+}
+
+func makeEducationProtoSchema() rules.Schema {
+	return rules.Schema{
 		Elements: []rules.DataElement{
 			{Name: "student", Type: rules.Proto{Protoname: "school.Student", Message: &school.Student{}}},
 			{Name: "now", Type: rules.Timestamp{}},
 			{Name: "self", Type: rules.Proto{Protoname: "school.HonorsConfiguration", Message: &school.HonorsConfiguration{}}},
 		},
 	}
+}
 
-	engine := cel.NewEngine()
-
-	rule := rules.Rule{
+func makeEducationProtoRules() rules.Rule {
+	return rules.Rule{
 		ID:     "student_actions",
-		Schema: schema,
+		Schema: makeEducationProtoSchema(),
 		Rules: map[string]rules.Rule{
-			"honor_student": {
+			"a": {
 				ID:   "honor_student",
 				Expr: `student.GPA >= self.Minimum_GPA && student.Status != school.Student.status_type.PROBATION && student.Grades.all(g, g>=3.0)`,
 				Self: &school.HonorsConfiguration{Minimum_GPA: 3.7},
 				Meta: true,
 			},
-			"at_risk": {
+			"b": {
 				ID:   "at_risk",
 				Expr: `student.GPA < 2.5 || student.Status == school.Student.status_type.PROBATION`,
 				Meta: false,
 			},
-			"tenure_gt_6months": {
+			"c": {
 				ID:   "tenure_gt_6months",
 				Expr: `now - student.EnrollmentDate > duration("4320h")`, // 6 months = 4320 hours
 				Meta: true,
@@ -226,11 +214,9 @@ func TestProtoMessage(t *testing.T) {
 		},
 	}
 
-	err := engine.AddRule(rule)
-	if err != nil {
-		t.Errorf("Error adding ruleset: %v", err)
-	}
+}
 
+func makeStudentProtoData() map[string]interface{} {
 	s := school.Student{
 		Age:            16,
 		GPA:            3.76,
@@ -240,47 +226,194 @@ func TestProtoMessage(t *testing.T) {
 		EnrollmentDate: &timestamp.Timestamp{Seconds: time.Date(2010, 5, 1, 12, 12, 59, 0, time.FixedZone("UTC-8", -8*60*60)).Unix()},
 	}
 
-	data := map[string]interface{}{
+	return map[string]interface{}{
 		"student": &s,
 		"now":     &timestamp.Timestamp{Seconds: time.Now().Unix()},
 	}
 
-	results, err := engine.Evaluate(data, "student_actions")
-	if err != nil {
-		t.Fatalf("Error evaluating: %v", err)
-	}
+}
+
+func TestProtoMessage(t *testing.T) {
+
+	is := is.New(t)
+	engine := cel.NewEngine()
+	err := engine.AddRule(makeEducationProtoRules())
+	is.NoErr(err)
+
+	results, err := engine.Evaluate(makeStudentProtoData(), "student_actions")
+	is.NoErr(err)
 	for _, v := range results.Results {
-		br, found := rule.Rules[v.RuleID]
-		if !found {
-			t.Errorf("Unexpected rule ID in results; no corresponding rule: %s", v.RuleID)
-		}
-		if br.Meta != v.Pass {
-			t.Errorf("Expected %t, got %t, rule %s", br.Meta, v.Pass, v.RuleID)
-		}
+		is.Equal(v.Meta, v.Pass)
 	}
 }
+
+func TestEvalOptions(t *testing.T) {
+
+	is := is.New(t)
+
+	cases := []struct {
+		opts []rules.Option      // Options to pass to evaluate
+		chk  func(*rules.Result) // Function to check the results
+	}{
+		{
+			opts: []rules.Option{rules.MaxDepth(0)},
+			chk: func(r *rules.Result) {
+				is.Equal(len(r.Results), 0) // No child results
+			},
+		},
+		{
+			opts: []rules.Option{rules.StopIfParentNegative(true)},
+			chk: func(r *rules.Result) {
+				is.Equal(len(r.Results), 0)
+			},
+		},
+		{
+			opts: []rules.Option{rules.StopIfParentNegative(false)},
+			chk: func(r *rules.Result) {
+				is.Equal(len(r.Results), 4)
+			},
+		},
+		{
+			opts: []rules.Option{rules.StopFirstPositiveChild(true)},
+			chk: func(r *rules.Result) {
+				i := 0
+				for _, v := range r.Results {
+					if v.Pass {
+						i++
+					}
+				}
+				is.Equal(i, 1)
+			},
+		},
+		{
+			opts: []rules.Option{rules.StopFirstNegativeChild(true)},
+			chk: func(r *rules.Result) {
+				i := 0
+				for _, v := range r.Results {
+					if !v.Pass {
+						i++
+					}
+				}
+				is.Equal(i, 1)
+			},
+		},
+		{
+			opts: []rules.Option{rules.StopFirstNegativeChild(true), rules.StopFirstPositiveChild(true)},
+			chk: func(r *rules.Result) {
+				is.Equal(len(r.Results), 1)
+			},
+		},
+		{
+			opts: []rules.Option{rules.ReturnFail(false), rules.ReturnPass(false)},
+			chk: func(r *rules.Result) {
+				is.Equal(len(r.Results), 0)
+			},
+		},
+		{
+			opts: []rules.Option{rules.ReturnPass(false)},
+			chk: func(r *rules.Result) {
+				is.Equal(len(r.Results), 2)
+			},
+		},
+
+		{
+			opts: []rules.Option{rules.ReturnFail(false)},
+			chk: func(r *rules.Result) {
+				is.Equal(len(r.Results), 2)
+			},
+		},
+	}
+
+	engine := cel.NewEngine()
+	err := engine.AddRule(makeEducationRules()...)
+	is.NoErr(err)
+
+	for _, c := range cases {
+		result, err := engine.Evaluate(makeStudentData(), "depthRules", c.opts...)
+		is.NoErr(err)
+		c.chk(result)
+	}
+}
+
+func TestRuleOptionOverride(t *testing.T) {
+
+	is := is.New(t)
+
+	cases := []struct {
+		opts []rules.Option      // Options to pass to evaluate
+		chk  func(*rules.Result) // Function to check the results
+	}{
+		{
+			opts: []rules.Option{},
+			chk: func(r *rules.Result) {
+				is.Equal(len(r.Results), 3) // rules a, b and E
+
+				// Check how many positives there are in rule D;
+				// the rule option should stop at the first positive
+				i := 0
+				for _, v := range r.Results["D"].Results {
+					if v.Pass {
+						i++
+					}
+				}
+				is.Equal(i, 1)
+
+				// Check how many positives there are in rule E;
+				// without a rule option, all should be returned
+				i = 0
+				for _, v := range r.Results["E"].Results {
+					if v.Pass {
+						i++
+					}
+				}
+				is.Equal(i, 2)
+			},
+		},
+	}
+
+	engine := cel.NewEngine()
+	err := engine.AddRule(makeEducationRules()...)
+	is.NoErr(err)
+
+	for _, c := range cases {
+		result, err := engine.Evaluate(makeStudentData(), "ruleOptions", c.opts...)
+		is.NoErr(err)
+		c.chk(result)
+	}
+}
+
+func TestRemoveRule(t *testing.T) {
+	is := is.New(t)
+
+	engine := cel.NewEngine()
+	err := engine.AddRule(makeEducationRules()...)
+	is.NoErr(err)
+
+	is.Equal(engine.RuleCount(), 3)
+
+	engine.RemoveRule("student_actions")
+	is.Equal(engine.RuleCount(), 2)
+}
+
+// ------------------------------------------------------------------------------------------
+// BENCHMARKS
+//
+//
+//
+//
+//
 
 func BenchmarkSimpleRule(b *testing.B) {
 
 	engine := cel.NewEngine()
-
-	education := rules.Schema{
-		Elements: []rules.DataElement{
-			{Name: "student.ID", Type: rules.String{}},
-			{Name: "student.Age", Type: rules.Int{}},
-			{Name: "student.GPA", Type: rules.Float{}},
-			{Name: "student.Status", Type: rules.String{}},
-			{Name: "student.Grades", Type: rules.List{ValueType: rules.String{}}},
-			{Name: "student.EnrollmentDate", Type: rules.String{}},
-			{Name: "now", Type: rules.String{}},
-		},
-	}
+	education := makeEducationSchema()
+	data := makeStudentData()
 
 	rule := rules.Rule{
 		ID:     "student_actions",
 		Schema: education,
 		Rules: map[string]rules.Rule{
-			"at_risk": {
+			"a": {
 				ID:     "at_risk",
 				Schema: education,
 				Expr:   `student.GPA < 2.5 || student.Status == "Probation"`,
@@ -293,17 +426,6 @@ func BenchmarkSimpleRule(b *testing.B) {
 		b.Errorf("Error adding ruleset: %v", err)
 	}
 
-	data := map[string]interface{}{
-		"student.ID":             "12312",
-		"student.Age":            16,
-		"student.GPA":            2.2,
-		"student.Status":         "Enrolled",
-		"student.Grades":         []interface{}{"A", "B", "A"},
-		"student.EnrollmentDate": "2018-08-03T16:00:00-07:00",
-		"now":                    "2019-08-03T16:00:00-07:00",
-		"alsoNow":                &timestamp.Timestamp{Seconds: time.Now().Unix()},
-	}
-
 	for i := 0; i < b.N; i++ {
 		engine.Evaluate(data, "student_actions")
 	}
@@ -312,24 +434,13 @@ func BenchmarkSimpleRule(b *testing.B) {
 func BenchmarkRuleWithArray(b *testing.B) {
 
 	engine := cel.NewEngine()
-
-	education := rules.Schema{
-		Elements: []rules.DataElement{
-			{Name: "student.ID", Type: rules.String{}},
-			{Name: "student.Age", Type: rules.Int{}},
-			{Name: "student.GPA", Type: rules.Float{}},
-			{Name: "student.Status", Type: rules.String{}},
-			{Name: "student.Grades", Type: rules.List{ValueType: rules.String{}}},
-			{Name: "student.EnrollmentDate", Type: rules.String{}},
-			{Name: "now", Type: rules.String{}},
-		},
-	}
+	education := makeEducationSchema()
 
 	rule := rules.Rule{
 		ID:     "student_actions",
 		Schema: education,
 		Rules: map[string]rules.Rule{
-			"honors_student": {
+			"a": {
 				ID:     "honors_student",
 				Schema: education,
 				Expr:   `student.GPA >= 3.6 && student.Status!="Probation" && !("C" in student.Grades)`,
@@ -342,17 +453,7 @@ func BenchmarkRuleWithArray(b *testing.B) {
 		b.Errorf("Error adding ruleset: %v", err)
 	}
 
-	data := map[string]interface{}{
-		"student.ID":             "12312",
-		"student.Age":            16,
-		"student.GPA":            2.2,
-		"student.Status":         "Enrolled",
-		"student.Grades":         []interface{}{"A", "B", "A"},
-		"student.EnrollmentDate": "2018-08-03T16:00:00-07:00",
-		"now":                    "2019-08-03T16:00:00-07:00",
-		"alsoNow":                &timestamp.Timestamp{Seconds: time.Now().Unix()},
-	}
-
+	data := makeStudentData()
 	for i := 0; i < b.N; i++ {
 		engine.Evaluate(data, "student_actions")
 	}
@@ -376,7 +477,7 @@ func BenchmarkProtoWithSelf(b *testing.B) {
 		ID:     "student_actions",
 		Schema: schema,
 		Rules: map[string]rules.Rule{
-			"at_risk": {
+			"a": {
 				ID:   "at_risk",
 				Expr: `student.GPA < self.Minimum_GPA || student.Status == school.Student.status_type.PROBATION`,
 				Self: &school.HonorsConfiguration{Minimum_GPA: 3.7},
@@ -427,7 +528,7 @@ func BenchmarkProtoWithoutSelf(b *testing.B) {
 		ID:     "student_actions",
 		Schema: schema,
 		Rules: map[string]rules.Rule{
-			"at_risk": {
+			"a": {
 				ID:   "at_risk",
 				Expr: `student.GPA < 2.5 || student.Status == school.Student.status_type.PROBATION`,
 				Meta: false,
@@ -458,4 +559,18 @@ func BenchmarkProtoWithoutSelf(b *testing.B) {
 		engine.Evaluate(data, "student_actions")
 	}
 
+}
+
+func BenchmarkCalculation(b *testing.B) {
+	engine := cel.NewEngine()
+
+	education := makeEducationSchema()
+	data := makeStudentData()
+
+	for i := 0; i < b.N; i++ {
+		_, err := engine.Calculate(data, `2.0+student.GPA + (1.344 * student.Adjustment)/3.3`, education)
+		if err != nil {
+			b.Fatalf("Could not calculate risk factor: %v", err)
+		}
+	}
 }
