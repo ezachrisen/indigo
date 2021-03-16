1d659134d87cc5752d9e650d41033895baf7430e
Espen Zachrisen <espen.zachrisen@alticeusa.com>
2021-03-15 23:47:52 -0500

    Saved m1 benchmark

--- run_tests.sh
+++ run_tests.sh
@@ -19,7 +19,7 @@ else
     echo "benchstat is installed; showing comparison with reference after completion"
     echo "   Results saved in ./testdata/benchmarks/after.txt"
     echo "   Update ./testdata/benchmarks/reference.txt to set a new 'before' point."
-    go test -run=XXX -bench=. ./... -count=10 > ./testdata/benchmarks/after.txt
+    go test -run=XXX -bench=. ./...  | tee ./testdata/benchmarks/after.txt
     benchstat ./testdata/benchmarks/reference.txt ./testdata/benchmarks/after.txt
 fi
 
