e13b3d20d8fa4652b2a6a429ea9edd5f7820d926
Espen Zachrisen <ezachrisen@gmail.com>
2021-04-23 03:36:57 +0000

    Added context cancelation to Eval; added a parsetype function to schema, and an example on how to load rules and schemas from a database

--- schema/schema.go
+++ schema/schema.go
@@ -1,6 +1,11 @@
 // Package schema defines the data types used by Indigo.
 package schema
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Schema defines the keys (variable names) and their data types used in a
 // rule expression. The same keys and types must be supplied in the data map
 // when rules are evaluated.
@@ -15,6 +20,25 @@ type Schema struct {
 	Meta interface{} `json:"-"`
 	// List of data elements supported by this schema
 	Elements []DataElement `json:"elements,omitempty"`
+}
+
+func (s *Schema) String() string {
+	x := strings.Builder{}
+	x.WriteString(s.ID)
+	if s.Name != "" {
+		x.WriteString("  '" + s.Name + "'")
+	}
+	x.WriteString("\n")
+	for _, e := range s.Elements {
+		x.WriteString(e.String())
+		x.WriteString("\n")
+	}
+
+	return x.String()
+}
+
+func (e *DataElement) String() string {
+	return fmt.Sprintf("  %s (%s)", e.Name, e.Type)
 }
 
 // DataElement defines a named variable in a schema
@@ -75,6 +99,106 @@ func (t Proto) String() string     { return "proto " + t.Protoname }
 // The value returned in the Result.
 // Inspect the Typ to determine what it is.
 type Value struct {
-	Val interface{}
-	Typ Type
+	Val  interface{}
+	Type Type
+}
+
+// ParseType parses a string that represents an Indigo type and returns the type.
+// The primitive types are their lower-case names (string, int, duration, etc.)
+// Maps and lists look like Go maps and slices:
+// Maps:  map[string]float
+// Lists: []string
+// Proto types look like this: proto(protoname)
+func ParseType(t string) (Type, error) {
+
+	if strings.Contains(t, "map") {
+		return parseMap(t)
+	}
+
+	if strings.Contains(t, "[]") {
+		return parseList(t)
+	}
+
+	if strings.Contains(t, "proto(") {
+		return parseProto(t)
+	}
+
+	switch t {
+	case "string":
+		return String{}, nil
+	case "int":
+		return Int{}, nil
+	case "float":
+		return Float{}, nil
+	case "bool":
+		return Bool{}, nil
+	case "duration":
+		return Duration{}, nil
+	case "timestamp":
+		return Timestamp{}, nil
+	case "any":
+		return Any{}, nil
+	default:
+		return Any{}, fmt.Errorf("unrecognized type: %s", t)
+	}
+}
+
+// parseMap parses a string and returns an Indigo map type.
+// The string must in the format map[<keytype]<valuetype>.
+// Example: map[string]int
+func parseMap(t string) (Type, error) {
+	startBracket := strings.Index(t, "[")
+	endBracket := strings.Index(t, "]")
+	if startBracket == -1 || endBracket == -1 || startBracket > endBracket || endBracket > len(t) {
+		return nil, fmt.Errorf("bad map specification")
+	}
+	keyTypeName := t[startBracket+1 : endBracket]
+	valueTypeName := t[endBracket+1:]
+	keyType, err := ParseType(keyTypeName)
+	if err != nil {
+		return Any{}, err
+	}
+	valueType, err := ParseType(valueTypeName)
+	if err != nil {
+		return Any{}, err
+	}
+
+	return Map{
+		KeyType:   keyType,
+		ValueType: valueType,
+	}, nil
+
+}
+
+// parseList parses a string and returns an Indigo list type.
+// The string must be in the format []<valuetype>
+// Example: []string
+func parseList(t string) (Type, error) {
+	typeName := t[2:]
+	valueType, err := ParseType(typeName)
+	if err != nil {
+		return Any{}, err
+	}
+
+	return List{
+		ValueType: valueType,
+	}, nil
+
+}
+
+// parseProto parses a string and returns a partial Indigo proto type.
+// The "Message" field of the proto struct must be suppplied later.
+// The string must be in the form proto(<protoname>).
+// Example: proto("school.Student")
+func parseProto(t string) (Type, error) {
+
+	startParen := strings.Index(t, "(")
+	endParen := strings.Index(t, ")")
+
+	if startParen == -1 || endParen == -1 || startParen > endParen || endParen > len(t) || endParen-startParen == 1 {
+		return nil, fmt.Errorf("bad proto specification")
+	}
+
+	name := t[startParen+1 : endParen]
+	return Proto{Protoname: name}, nil
 }
