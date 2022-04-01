8b6210aa8f16db27ba928499581e3650fb535ef7
Espen Zachrisen <ezachrisen@gmail.com>
2022-04-01 02:47:10 +0000

    Updated to go 1.18

--- run_tests.sh
+++ run_tests.sh
@@ -6,12 +6,12 @@
 echo "----- Running benchmarks"
 if ! [ -x "$(command -v benchstat)" ]
 then
-    go test -run=XXX -bench=. ./...
+    go test -run=XXX -bench=.  ./...
 else
     echo "benchstat is installed; showing comparison with reference after completion"
     echo "   Results saved in ./testdata/benchmarks/after.txt"
     echo "   Update ./testdata/benchmarks/reference.txt to set a new 'before' point."
-    go test -run=XXX -bench=.  ./... | tee ./testdata/benchmarks/after.txt
+    go test -run=XXX -bench=. -count 5 ./... | tee ./testdata/benchmarks/after.txt
     benchstat ./testdata/benchmarks/reference.txt ./testdata/benchmarks/after.txt
 fi
 
